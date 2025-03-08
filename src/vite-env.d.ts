8f91c263467acfa30c81075d772291edbe8ab798
Thushar Vijay <141836999+thusharvijay@users.noreply.github.com>
2025-03-09 01:35:04 +0530

    Add files via upload

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+interface Window {
+    ethereum?: {
+      request: (args: { method: string; params?: any[] }) => Promise<any>;
+      on: (event: string, callback: (params: any) => void) => void;
+      removeListener: (event: string, callback: (params: any) => void) => void;
+    };
+  }
